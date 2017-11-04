79a12b51fe2ff1cc9f95d3a2fe0655120c6b6e49
Valery Korenkov <vnk@anadeainc.com>
2017-11-04 16:21:43 +0200

    Add methods for BasePage

--- src/main/java/pageobjects/BasePage.java
+++ src/main/java/pageobjects/BasePage.java
@@ -5,6 +5,8 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
     protected static AppiumDriver<MobileElement> driver;
@@ -14,4 +16,19 @@ public abstract class BasePage {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
         this.driver = driver;
     }
+
+    public void hideKeyboard() {
+        driver.hideKeyboard();
+    }
+
+    public void returnBack() {
+        driver.navigate().back();
+    }
+
+    public void waitForElement(MobileElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public abstract BasePage initializePage();
 }
